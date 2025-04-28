44fede75a871a4e36d38e3783e475f3922ca3204
AV48 <anushkavij721@gmail.com>
2025-04-28 18:05:55 -0500

    Create humming.js

---
+++ humming.js
@@ -0,0 +1,63 @@
+var images = document.getElementById("hummingimages");
+var button = document.getElementById("start-simulation");
+button.addEventListener("click", runSimulation);
+
+function runSimulation() {
+setTimeout(() => {
+  images.src = "photos/box breathing (16).jpg"
+}, 500);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated.jpg";
+}, 3000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated (1).jpg";
+}, 4000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated (2).jpg";
+}, 5000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated (3).jpg";
+}, 6000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated (4).jpg";
+}, 7000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing updated (5).jpg";
+}, 8000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (6).jpg";
+}, 9000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (7).jpg";
+}, 10000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (8).jpg";
+}, 11000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (9).jpg";
+}, 12000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (10).jpg";
+}, 13000);
+
+setTimeout(() => {
+  images.src = "photos/humming bee breathing (11).jpg";
+}, 14000);
+
+setTimeout(() => {
+  images.src = "photos/box breathing (15).jpg";
+}, 15000);
+}
+
+
