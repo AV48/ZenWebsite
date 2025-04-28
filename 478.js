4b9b527abac9c30c784a63f26dee1a9576a6a7ff
AV48 <anushkavij721@gmail.com>
2025-04-28 18:00:14 -0500

    Create 478.js

---
+++ 478.js
@@ -0,0 +1,92 @@
+var images = document.getElementById("fourseveneightimages");
+var button = document.getElementById("start-simulation");
+button.addEventListener("click", runSimulation);
+
+function runSimulation() {
+  setTimeout(() => {
+     images.src = "photos/box breathing (4).jpg";
+    }, 500);
+  
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing.jpg";
+  }, 4000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (1).jpg";
+  }, 5000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (2).jpg";
+  }, 6000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (3).jpg";
+  }, 7000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (5).jpg";
+  }, 8000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (6).jpg";
+  }, 9000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (7).jpg";
+  }, 10000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (8).jpg";
+  }, 11000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (9).jpg";
+  }, 12000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (10).jpg";
+  }, 13000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (11).jpg";
+  }, 14000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (12).jpg";
+  }, 15000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (13).jpg";
+  }, 16000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (14).jpg";
+  }, 17000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (15).jpg";
+  }, 18000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (16).jpg";
+  }, 19000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (17).jpg";
+  }, 20000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (18).jpg";
+  }, 21000);
+
+  setTimeout(() => {
+    images.src = "478 breathing/478 breathing (19).jpg";
+  }, 22000);
+
+  setTimeout(() => {
+    images.src = "photos/box breathing (17).png";
+  }, 23000);
+  
+}
+
+
